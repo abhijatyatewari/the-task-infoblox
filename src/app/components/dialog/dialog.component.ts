c0121a9824c1db2c14282e71ed89afd074dba5b7
Abhijat Tewari <Abhijat_Tewari@epam.com>
2023-03-14 01:08:11 +0530

    NAT space disabled for Edit and IP validation

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -17,10 +17,12 @@ import { TasksService } from '../../services/task.service';
 export class DialogComponent {
   public natSpaces = natSpaces;
   dialogTitle: string = this.data.task?.name ? 'Edit '+ this.data.task.name : 'Create Server';
-  taskName: string = this.data.task.name ?? '';
-  taskdescription: string = this.data.task.description ?? '';
-  taskNatSpace: string = this.data.task.nat_space_id ?? '';
-  taskIpAddress: string = this.data.task.server_ip ?? '';
+  selectorDisabled: boolean = this.data.task?.name ? true : false;
+  taskName: string = this.data.task?.name ?? '';
+  taskdescription: string = this.data.task?.description ?? '';
+  taskNatSpace: string = this.data.task?.nat_space_id ?? '';
+  taskIpAddress: string = this.data.task?.server_ip ?? '';
+  showIpValidationError: boolean = false;
   constructor(public dialogRef: DialogRef<DialogComponent>, @Inject(DIALOG_DATA) public data: any, private tasksService : TasksService) {}
   
   closeDialog() {
@@ -28,8 +30,20 @@ export class DialogComponent {
     console.log(this.data, this.taskName);
 
   }
+  isIpValid () : boolean {
+    let ipv4 = /(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])/;
+     if (this.taskIpAddress.match(ipv4))
+     return true;
+     else
+     return false;
+  }
 
   saveTask() {
+    if(!this.isIpValid()) {
+      this.showIpValidationError = true;
+      return;
+    }
+    this.showIpValidationError = false;
     let task : TaskDetails =  { name : this.taskName, description : this.taskdescription, nat_space_id : this.taskNatSpace, server_ip : this.taskIpAddress};
     this.tasksService.editTask(task);
     this.dialogRef.close();
